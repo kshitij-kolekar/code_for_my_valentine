d474db6bf2d003cfc9fc83e93f786ba88dcd0001
Kshitij_Kolekar <kshitijkolekar111@gmail,com>
2025-02-09 13:52:05 +0530

    Added Hero Section

--- home_functions.js
+++ home_functions.js
@@ -15,4 +15,18 @@ window.addEventListener("scroll", function () {
   }, { threshold: 0.3 });
 
   poemLines.forEach(line => observer.observe(line));
+
+  const poemLines1 = document.querySelectorAll(".heropoem");
+
+  const observer1 = new IntersectionObserver(entries => {
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          entry.target.classList.toggle("visible", window.scrollY > 50);
+        }, index * 400);
+      }
+    });
+  }, { threshold: 0.3 });
+
+  poemLines1.forEach(line => observer1.observe(line));
 });
